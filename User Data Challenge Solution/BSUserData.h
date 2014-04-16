97dee108541e7eeaf41af6284a3f21327d5c677e
Brady Simmons <brady@bradysismacbook.gateway.2wire.net>
2014-04-15 19:49:35 -0500

    first commit

---
+++ User Data Challenge Solution/BSUserData.h
@@ -0,0 +1,13 @@
+//
+//  BSUserData.h
+//  User Data Challenge Solution
+//
+//  Created by Brady Simmons on 4/13/14.
+//  Copyright (c) 2014 Lorux. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BSUserData : NSObject
+
+@end
---
+++ User Data Challenge Solution/BSUserData.m
@@ -0,0 +1,13 @@
+//
+//  BSUserData.m
+//  User Data Challenge Solution
+//
+//  Created by Brady Simmons on 4/13/14.
+//  Copyright (c) 2014 Lorux. All rights reserved.
+//
+
+#import "BSUserData.h"
+
+@implementation BSUserData
+
+@end
